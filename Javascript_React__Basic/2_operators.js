930b3d1ab7086a88b00874f837cf92984e882186
Misol Goh <melancholy8914@gmail.com>
2020-07-08 21:50:32 +0200

    add day 2 code

---
+++ Javascript_React__Basic/2_operators.js
@@ -0,0 +1,18 @@
+/*
+  Please complete 'solve' function to know how much you have to pay for the meal
+  Note: You can find the total cost by computing;
+   - the meal price (base cost of a meal)
+   - tip percent (the percentage of the meal price being added as tip)
+   - tax percent (the percentage of the meal price being added as tax)
+**/
+
+// Complete the solve function below
+function solve(mealCost, tipPercent, taxPercent) {
+
+}
+
+const mealCost = 12;
+const tipPercent = 20;
+const taxPercent = 8;
+
+solve(mealCost, tipPercent, taxPercent);
---
+++ Javascript_React__Basic_answer/2_operators.js
@@ -0,0 +1,21 @@
+/*
+  Please complete 'solve' function to know how much you have to pay for the meal
+  Note: You can find the total cost by computing;
+   - the meal price (base cost of a meal)
+   - tip percent (the percentage of the meal price being added as tip)
+   - tax percent (the percentage of the meal price being added as tax)
+**/
+
+// Complete the solve function below
+function solve(mealCost, tipPercent, taxPercent) {
+  const tipCost = mealCost * tipPercent / 100;
+  const taxCost = mealCost * taxPercent / 100;
+
+  const totalCost = Math.round(mealCost + tipCost + taxCost);
+
+  console.log(totalCost);
+}
+
+solve(12, 20, 8);  // 15
+
+solve(15.50, 15, 10); // 19
