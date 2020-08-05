e43784b373976916b595af5cf573a5f0cf44e631
Misol Goh <melancholy8914@gmail.com>
2020-08-05 23:56:46 +0200

    add Day 6: first review question

---
+++ Javascript_React__Basic/6_review_1.js
@@ -0,0 +1,12 @@
+/**
+ * Given a string S, of length N that is indexed from 0 to N - 1, print it's even-indexed characters as 2 space-separated strings on a single line.
+ * Note: 0 is considered as an even index
+ */
+
+// Complete the function
+function separateString(str) {
+
+}
+
+separateString('Hacker');
+separateString('Rank');
---
+++ Javascript_React__Basic_answer/6_review_1.js
@@ -0,0 +1,26 @@
+/**
+ * Given a string S, of length N that is indexed from 0 to N - 1, print it's even-indexed characters as 2 space-separated strings on a single line.
+ * Note: 0 is considered as an even index
+ */
+
+// Complete the function
+function separateString(str) {
+  if (!str || typeof str !== 'string') {
+    return;
+  }
+
+  const evens = [];
+  const odds = [];
+  for(let i = 0; i < str.length ; i++) {
+    if (i % 2 === 0) {
+      evens.push(str[i]);
+    } else {
+      odds.push(str[i]);
+    }
+  }
+
+  console.log(`${evens.join('')} ${odds.join('')}`);
+}
+
+separateString('Hacker');
+separateString('Rank');
